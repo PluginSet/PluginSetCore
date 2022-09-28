effad4c93435d346c281341e3f5498e8e62df87a
LyneXiao <lixonxs@icloud.com>
2022-09-28 16:16:32 +0800

    调整SDK

--- Runtime/Common/SerializableDict.cs
+++ Runtime/Common/SerializableDict.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PluginSet.Core
@@ -19,10 +20,32 @@ namespace PluginSet.Core
     }
 
     [Serializable]
-    public struct SerializableDict<K, V>
+    public class SerializableDict<K, V>
     {
         [SerializeField]
         public SerializableKeyValuePair<K, V>[] Pairs;
+
+        public SerializableDict()
+        {
+            
+        }
+        
+        public SerializableDict(Dictionary<K, V> dictionary)
+        {
+            Pairs = new SerializableKeyValuePair<K, V>[dictionary.Count];
+            var iter = dictionary.GetEnumerator();
+            var index = 0;
+            while (iter.MoveNext())
+            {
+                var kv = iter.Current;
+                Pairs[index++] = new SerializableKeyValuePair<K, V>()
+                {
+                    Key = kv.Key,
+                    Value = kv.Value,
+                };
+            }
+            iter.Dispose();
+        }
 
         public override string ToString()
         {
