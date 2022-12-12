5ca96f536c756dc408b512ecc3d7be0088bf3010
LyneXiao <lixonxs@icloud.com>
2022-12-12 13:17:30 +0800

    调整SDK

---
+++ Runtime/Common/EnumeratorLocker.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+namespace PluginSet.Core
+{
+    public class EnumeratorLocker: CustomYieldInstruction
+    {
+        private bool _keepWaiting = false;
+        public override bool keepWaiting => _keepWaiting;
+
+        public void Lock()
+        {
+            _keepWaiting = true;
+        }
+
+        public void Unlock()
+        {
+            _keepWaiting = false;
+        }
+
+        public void UnlockDelay(float seconds)
+        {
+            CoroutineHelper.Instance.StartCoroutine(UnlockDelayInternal(seconds));
+        }
+
+        private IEnumerator UnlockDelayInternal(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            Unlock();
+        }
+    }
+}
