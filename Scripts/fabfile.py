12bce4cdf3588e15b82398934b8eec3e78b47a76
LyneXiao <lixonxs@icloud.com>
2023-03-24 18:11:27 +0800

    打包调整

--- Scripts/fabfile.py
+++ Scripts/fabfile.py
@@ -1685,7 +1685,6 @@ def buildUploadFlow(context, platform, channelIds, out_path
         channel = build_result.get("channel", None)
         version_name = build_result.get("version", None)
         build_number = build_result.get("build", None)
-        patches_path = build_result.get("patchesPath", None)
         if channel is None or version_name is None or build_number is None:
             return FAILURE("Error build result :" + str(build_result))
 
@@ -1702,7 +1701,8 @@ def buildUploadFlow(context, platform, channelIds, out_path
             else:
                 raise Exception("not support platform " + platform)
 
-        if patches_path:
+        if build_result.__contains__("patchesPath"):
+            patches_path = os.path.join(out_path, "Patches")
             upload_patches(patches_path, id, secret, bucketname, endpoint, resources_key)
         
         write_all_file_versions(build_result, channelIds, id, secret, bucketname, endpoint, version_file, streamtemplate, patchtemplate
